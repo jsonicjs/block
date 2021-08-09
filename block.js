d0f55e5d23d04f9c88fb6100ecadf0ae00815412
rjrodger <richard@ricebridge.com>
2021-08-09 17:49:48 +0100

    test coverage

--- block.js
+++ block.js
@@ -1,6 +1,68 @@
 
-function Block(jsonic) {
-  jsonic.options({plugin:{block:{x:1}}})
+
+
+function Block(jsonic, options) {
+  let { regexp, escre } = jsonic.util
+  
+  jsonic.lex(function makeBlockMatcher() {
+    let blockre = regexp(
+      's',
+      '^(',
+      Object
+        .entries(options.marker)
+        .filter(([sm,em]) => 'string'===typeof(sm) && 'string'===typeof(em))
+        .map(([sm,em])=>
+             (escre(sm)+'(.*?)('+escre(em)+')'))
+        .join('|'),
+      ')'
+    )
+
+    return function blockMatcher(lex) {
+      let pnt = lex.pnt
+      let fwd = lex.src.substring(pnt.sI)
+
+      let m = fwd.match(blockre)
+      
+      if (m) {
+        let msrc = m[0]
+        let mlen = msrc.length
+
+        if (0 < mlen) {
+          let mf = m.filter(p=>null!=p)
+
+          let txt = mf[2]
+              .replace(/^\s*\r?\n/,'')
+              .replace(/\r?\n\s*$/,'')
+
+          let indent = (txt.match(/^(\s+)/)||['',''])[1]
+
+          txt = txt
+            .substring(indent.length)
+            .replace(regexp('g','(\\r?\\n)',indent),'$1')
+          
+          let tkn = lex.token(jsonic.token.TX, txt, msrc, pnt)
+
+          let rows = msrc.replace(/[^\n]/g,'').length
+          pnt.sI += msrc.length
+          pnt.rI += rows
+          pnt.cI = (0<rows?1:pnt.cI)+(msrc.match(/[^\r\n]*$/)||[''])[0].length
+
+          return tkn
+        }
+      }
+    }
+  })
+
+  
+}
+
+
+Block.defaults = {
+   
+  // Block markers
+  marker: {
+    "'''": "'''",
+  },
 }
 
 
--- test/block.test.js
+++ test/block.test.js
@@ -11,7 +11,85 @@ describe('block', () => {
 
   test('happy', () => {
     expect(j('{"a":1}')).toEqual({ a: 1 })
-    console.log('PO',j.options.plugin)
+
+    expect(j.options.plugin.Block).toEqual({marker: { "'''": "'''" }})
+
+    expect(j(`{a:'''foo'''}`)).toEqual({ a: 'foo' })
+
+    expect(j(`{a:\n'''\nfoo\nbar\n'''}`)).toEqual({ a: 'foo\nbar' })
+
+    expect(j(`
+{
+  a: '''
+  foo\t
+    bar\t
+  '''
+}
+`)).toEqual({ a: 'foo\t\n  bar\t' })
   })
 
+
+  test('multiple', () => {
+    const jm = Jsonic.make().use(Block, {
+      marker: {
+        '#<': '>#',
+        '{{': '}}',
+        "'''": null, // Turns off '''
+      }
+    })
+    
+    expect(jm('{"a":1}')).toEqual({ a: 1 })
+
+    expect(jm.options.plugin.Block).toEqual({marker: {
+      '#<': '>#',
+      '{{': '}}',
+      "'''": null,
+    }})
+
+    expect(jm(`{a:#<foo>#}`)).toEqual({ a: 'foo' })
+    expect(jm(`{b:{{bar}}}`)).toEqual({ b: 'bar' })
+    expect(jm(`{a:#<foo>#, b:{{bar}}}`)).toEqual({ a: 'foo', b: 'bar' })
+    expect(jm(`{a:#<\nfoo\n>#, b:{{  \n  bar\n   }}}`))
+      .toEqual({ a: 'foo', b: 'bar' })
+  })
+
+  
+  
+  test('point', () => {
+    expect(()=>j(`{a:'''foo''']`)).toThrow(/unexpected.*:1:13/s)
+    expect(()=>j(`{a:\n'''foo''']`)).toThrow(/unexpected.*:2:10/s)
+    expect(()=>j(`{a:\n'''\nfoo''']`)).toThrow(/unexpected.*:3:7/s)
+    expect(()=>j(`{a:\n'''\nfoo\n''']`)).toThrow(/unexpected.*:4:4/s)
+
+    expect(()=>j(`{a: '''foo''']`)).toThrow(/unexpected.*:1:14/s)
+    expect(()=>j(`{a:\n '''foo''']`)).toThrow(/unexpected.*:2:11/s)
+    expect(()=>j(`{a:\n '''\nfoo''']`)).toThrow(/unexpected.*:3:7/s)
+    expect(()=>j(`{a:\n '''\nfoo\n''']`)).toThrow(/unexpected.*:4:4/s)
+
+    expect(()=>j(`{a: ''' foo''']`)).toThrow(/unexpected.*:1:15/s)
+    expect(()=>j(`{a:\n ''' foo''']`)).toThrow(/unexpected.*:2:12/s)
+    expect(()=>j(`{a:\n '''\n foo''']`)).toThrow(/unexpected.*:3:8/s)
+    expect(()=>j(`{a:\n '''\n foo\n''']`)).toThrow(/unexpected.*:4:4/s)
+
+    expect(()=>j(`{a: ''' foo ''']`)).toThrow(/unexpected.*:1:16/s)
+    expect(()=>j(`{a:\n ''' foo ''']`)).toThrow(/unexpected.*:2:13/s)
+    expect(()=>j(`{a:\n '''\n foo ''']`)).toThrow(/unexpected.*:3:9/s)
+    expect(()=>j(`{a:\n '''\n foo \n''']`)).toThrow(/unexpected.*:4:4/s)
+
+    expect(()=>j(`{a:\n '''\n foo \n ''']`)).toThrow(/unexpected.*:4:5/s)
+
+    expect(()=>j(`{a: ''' foo ''' ]`)).toThrow(/unexpected.*:1:17/s)
+    expect(()=>j(`{a:\n ''' foo ''' ]`)).toThrow(/unexpected.*:2:14/s)
+    expect(()=>j(`{a:\n '''\n foo ''' ]`)).toThrow(/unexpected.*:3:10/s)
+    expect(()=>j(`{a:\n '''\n foo \n ''' ]`)).toThrow(/unexpected.*:4:6/s)
+
+    expect(()=>j(`{a: ''' foo '''\n ]`)).toThrow(/unexpected.*:2:2/s)
+    expect(()=>j(`{a:\n ''' foo '''\n ]`)).toThrow(/unexpected.*:3:2/s)
+    expect(()=>j(`{a:\n '''\n foo '''\n ]`)).toThrow(/unexpected.*:4:2/s)
+    expect(()=>j(`{a:\n '''\n foo \n '''\n ]`)).toThrow(/unexpected.*:5:2/s)
+
+    
+  })
+  
+  
 })
